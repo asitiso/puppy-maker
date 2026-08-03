30e1e7070cf6bf52de8a9b16b63d4144a7853bad
asitiso <asitiso.woo@gmail.com>
2026-08-03 15:22:18 +0900

    feat: connect layered home to existing game

---
+++ src/Root.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import App from './App';
+import LayeredHome from './LayeredHome';
+import './layered-home.css';
+
+export default function Root() {
+  const [showHome, setShowHome] = useState(true);
+
+  const openSchedule = () => {
+    const scheduleButton = document.querySelector<HTMLButtonElement>('.hub-screen .bottom-nav button');
+    scheduleButton?.click();
+    setShowHome(false);
+  };
+
+  return <>
+    <App />
+    {showHome && <LayeredHome onSchedule={openSchedule} />}
+  </>;
+}
