12277636b4d2260b0d5ce858d549988e8b8e62c4
asitiso <asitiso.woo@gmail.com>
2026-08-08 12:04:29 +0900

    style: load schedule synergy layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Root from './Root';
 import './styles.css';
 import './lobby-art.css';
+import './schedule-synergy.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><Root /></React.StrictMode>
