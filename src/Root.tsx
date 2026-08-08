3f509b2be809905980a13b4af282467c0c8385c4
asitiso <asitiso.woo@gmail.com>
2026-08-09 06:36:01 +0900

    feat: surface long-run progress on layered home

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,6 @@
 import { GameApp, useGameState } from './App';
 import LayeredHome from './LayeredHome';
+import { ProgressRibbon } from './components/ProgressRibbon';
 import './layered-home.css';
 import './home-panels.css';
-
-export default function Root() {
-  const { state, dispatch } = useGameState();
-  const showHome = state.screen === 'hub';
-
-  return <>
-    <GameApp state={state} dispatch={dispatch} renderHub={!showHome} />
-    {showHome && <LayeredHome state={state} onSchedule={() => dispatch({ type: 'GO', screen: 'schedule' })} />}
-  </>;
-}
+export default function Root(){const{state,dispatch}=useGameState();const showHome=state.screen==='hub';return <><GameApp state={state} dispatch={dispatch} renderHub={!showHome}/>{showHome&&<><LayeredHome state={state} onSchedule={()=>dispatch({type:'GO',screen:'schedule'})}/><ProgressRibbon state={state}/></>}</>}
