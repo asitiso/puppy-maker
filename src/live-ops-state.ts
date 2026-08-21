390f802bfbf50c89771834ad8db1b825a0edbe9d
asitiso <asitiso.woo@gmail.com>
2026-08-21 18:04:45 +0900

    fix: reject impossible season hydration years

--- src/live-ops-state.ts
+++ src/live-ops-state.ts
@@ -21,8 +21,8 @@ export type LiveOpsPersistentState = {
   claimedSeasonKeepsakeMilestones?:SeasonKeepsakeMilestoneId[];
 };
 
-const seasonKeyPattern = /^\d+-(spring|summer|autumn|winter)$/;
-const seasonTierPattern = /^\d+-(spring|summer|autumn|winter):(10|[1-9])$/;
+const seasonKeyPattern = /^[1-9]\d*-(spring|summer|autumn|winter)$/;
+const seasonTierPattern = /^[1-9]\d*-(spring|summer|autumn|winter):(10|[1-9])$/;
 const weekKeyPattern = /^[1-9]\d*-(?:[1-9]|1[0-2])-[1-4]$/;
 const weeklyRewardPattern = /^[1-9]\d*-(?:[1-9]|1[0-2])-[1-4]:(steady_training|field_patrol|warm_bond|guardian_sortie|elite_clear|deep_training|adventure_week|gift_week)$/;
 const directiveIdPattern = /^(steady_training|field_patrol|warm_bond|guardian_sortie|elite_clear|deep_training|adventure_week|gift_week)$/;
